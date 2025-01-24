889355b82dd1d21c3d3c133c25b34924f12718c2
Braun Roland <rolibraun94@gmail.com>
2025-01-24 10:32:44 +0100

    Add project files.

---
+++ EmberOOP2/Ember.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmberOOP2
+{
+    public class Ember
+    {
+        private string nev;
+        private int szuletesiEv;
+        private int magassag;
+
+
+
+    }
+}
